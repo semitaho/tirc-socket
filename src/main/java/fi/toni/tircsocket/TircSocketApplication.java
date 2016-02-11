baac1aac86ce5a4242443ea8f0d627b6510be3fb
Toni Aho <toni.aho@siili.fi>
2016-02-11 21:32:34 +0200

    first commit

---
+++ src/main/java/fi/toni/tircsocket/TircSocketApplication.java
@@ -0,0 +1,21 @@
+package fi.toni.tircsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by taho on 11/02/16.
+ */
+
+@SpringBootApplication
+public class TircSocketApplication {
+
+  public static void main(String[] args) {
+    ApplicationContext ctx = SpringApplication.run(TircSocketApplication.class, args);
+
+
+  }
+
+
+}
