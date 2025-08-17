0bec08262cdb7c65d2b1df5227685c84b10deba7
Toni Aho <toni@tahoo.fi>
2025-08-17 17:54:42 +0300

    Korjaus

--- src/main/java/fi/toni/tircsocket/dto/Message.java
+++ src/main/java/fi/toni/tircsocket/dto/Message.java
@@ -2,20 +2,20 @@ package fi.toni.tircsocket.dto;
 
 import com.google.cloud.firestore.DocumentSnapshot;
 
-public record Message(Long time, String nick, String type, String comment, String source) {
+public record Message(Long time, String nick, String type, String line, String source) {
 
   public static Message fromDocument(final DocumentSnapshot documentSnapshot) {
     return new Message(
             documentSnapshot.getLong("time"),
             documentSnapshot.getString("nick"),
             documentSnapshot.getString("type"),
-            documentSnapshot.getString("comment"),
+            documentSnapshot.getString("line"),
             documentSnapshot.getString("source")
 
     );
   }
 
   public String toSocketMessage(final String channel) {
-    return "PRIVMSG %s :(%s): %s".formatted(channel, nick(), comment());
+    return "PRIVMSG %s :(%s): %s".formatted(channel, nick(), line());
   }
 }
